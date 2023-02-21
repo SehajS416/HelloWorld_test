4576ebc4d4faa6c971e7e636757c2c4df6fcfee8
SehajS416 <61622813+SehajS416@users.noreply.github.com>
2023-02-21 09:31:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 class HelloWorld{
 	public static void main(String args[]){
 			System.out.println("Hello World");
-			System.out.println("Test");
-		        System.out.println("Testing again");
-
+			System.out.printl("Test the webhook");
+		        
 	}
 	}
