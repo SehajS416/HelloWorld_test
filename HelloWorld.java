d975c24ba5d9bffe5143ac8f603f43eccdd3c051
SehajS416 <61622813+SehajS416@users.noreply.github.com>
2023-02-21 15:10:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ class HelloWorld{
 	public static void main(String args[]){
 			System.out.printl("This is a Hello World Program in Java");
 		         System.out.println("Test");
-		         System.out.println("Test1");
 		          System.out.println("Test2");
 	}
 	}
